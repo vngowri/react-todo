7ed1f3f7e1af4871cb21c690b97e72ef7ba051d8
gowri <vngowri@gmail.com>
2017-06-06 16:03:06 -0500

    Added todosReducer and tests

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,61 +1,75 @@
-    var React = require('react');
-    var TodoSearch = require('TodoSearch');
-    var TodoList = require('TodoList');
-    var AddTodos = require('AddTodos');
+var React = require('react');
+var uuid = require('node-uuid');
+var moment = require('moment');
 
-    var TodoApp = React.createClass({
-        getInitialState: function () {
-            return {
-                showCompleted: false,
-                searchText: '',
-              todos: [{
-                  id: 1,
-                  text: 'Walk the dog'
-              }, {
-                  id: 2,
-                  text: 'Clean the yard'
-              }, {
-                  id: 3,
-                  text: 'Finish Laundry'                  
-              }, {
-                  id: 4,
-                  text: 'Run Errands'                  
-              }, {
-                  id: 5,
-                  text: 'Get haircut'              
-              }]  
-            };
-        },
-    
-        handleAddTodo: function(todoText) {
-            alert('new todo: ' + todoText);
-            var newTodo = {
-                id: this.state.todos.length+1,
-                text: todoText
-            };
-            this.state.todos.push(newTodo);
-            this.setState({todos:this.state.todos});             
-        },
-        
-        handleTodoSearch: function(showCompleted, searchText) {
-            this.setState({
-                showCompleted: showCompleted,
-                searchText: searchText.toLowerCase()
-            });
-            
-        },
-        
-      render: function () {        
-        var {todos} = this.state;
-        return (
-            <div>
-                <TodoSearch onSearch={this.handleTodoSearch}/>
-                <TodoList todos={todos}/>
-                <AddTodos onAddTodo={this.handleAddTodo}/>
-            </div>
-        )
+var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
+var TodoAPI = require('TodoAPI');
+
+var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      showCompleted: false,
+      searchText: '',
+      todos: TodoAPI.getTodos()
+    };
+  },
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
+  },
+  handleAddTodo: function (text) {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ]
+    });
+  },
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
 
+      return todo;
     });
 
-    module.exports = TodoApp;
+    this.setState({todos: updatedTodos});
+  },
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
+  render: function () {
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+
+    return (
+      <div>
+        <h1 className="page-title">Todo App</h1>
+
+        <div className="row">
+          <div className="column small-centered small-11 medium-6 large-5">
+            <div className="container">
+              <TodoSearch onSearch={this.handleSearch}/>
+              <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+              <AddTodo onAddTodo={this.handleAddTodo}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -2,19 +2,28 @@ var React = require('react');
 var Todo = require('Todo');
 
 var TodoList = React.createClass({
-   render: function () {       
-       var {todos}  = this.props;
-       var renderTodos = ()=> {
-         return todos.map((todo) => {
-             return (                
-                <Todo key={todo.id}{...todo}/>                 
-             );         
-        });
-       };
+  render: function () {
+    var {todos} = this.props;
+    var renderTodos = () => {
+      if (todos.length === 0) {
         return (
-          <div>{renderTodos()}</div>
-        )
-      }    
+          <p className="container__message">Nothing To Do</p>
+        );
+      }
+
+      return todos.map((todo) => {
+        return (
+          <Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>
+        );
+      });
+    };
+
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    )
+  }
 });
 
 module.exports = TodoList;
